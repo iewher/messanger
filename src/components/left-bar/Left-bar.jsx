edc1f2bc6eec75155616873e8916ea82c370fcfc
iewher <goshagang@mail.ru>
2023-05-27 18:51:12 +0500

    added chat contacts

--- src/components/left-bar/Left-bar.jsx
+++ src/components/left-bar/Left-bar.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style/left-bar-style.css'
 
 import { Header } from '../header/Header';
 import { Search } from '../search/Search';
-import  GET_API   from '../GET_API/GET_API';
 
 export const LeftBar = () => {
+    const [chatid, setChatId] = useState('');
+    const [chats, setChats] = useState([]);
+
+    const handleInputChange = (event) => {
+        setChatId(event.target.value);
+    };
+
+    const handleSearchButtonClick = () => {
+        setChats([...chats, chatid]);
+        setChatId('');
+    };
+
     return (
         <div className="left-bar">
             <Header />
-            <Search />
+            <Search onChange={handleInputChange} onSearchButtonClick={handleSearchButtonClick} />
             <div className="left-bar-content">
-                Создайте чат, и он будет отображаться
+                {chats.map((chat, index) => (
+                    <div key={index}>
+                        <button>{chat}</button>
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './style/search-style.css';
 
-export const Search = () => {
+export const Search = ({ onChange, onSearchButtonClick }) => {
+    const handleInputChange = (event) => {
+        if (onChange) {
+            onChange(event);
+        }
+    }
+
+    const handleSearchButtonClick = () => {
+        if (onSearchButtonClick) {
+            onSearchButtonClick();
+        }
+    }
+
     return (
         <div className="search">
-            <input type="text" placeholder='Поиск или новый чат' className='search-input' />
+            <input type="text" placeholder='Поиск или новый чат' className='search-input' onChange={handleInputChange} />
+            <button onClick={handleSearchButtonClick} className='search-button'>Поиск</button>
         </div>
     )
 }
