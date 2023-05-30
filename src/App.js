a7bf8cb017156f83ad9edb098fd88e9ca8801714
iewher <goshagang@mail.ru>
2023-05-30 14:08:40 +0500

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Login } from './components/login-page/Login';
 import { Main } from './components/main/Main';
